a08754b8dbe036f21d5cfcfb82521062f2e13413
Eric Bomgardner <eric.bomgardner@gmail.com>
2017-10-03 18:49:10 -0700

    Adding TODO to better detect URLs

--- Tabless/String+WebURL.swift
+++ Tabless/String+WebURL.swift
@@ -1,5 +1,11 @@
 extension String {
+    /// Returns whether the string looks like a web URL
+    /// TODO(#2): Better detect URLs
     func isWebURL() -> Bool {
-        return hasPrefix("www.") || contains(".com") || contains(".net") || contains(".org")
+        return hasPrefix("http")
+            || hasPrefix("www.")
+            || contains(".com")
+            || contains(".net")
+            || contains(".org")
     }
 }
