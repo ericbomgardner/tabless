0b43af3fd2adc3ade9224133748638af105cedc2
Eric Bomgardner <eric.bomgardner@gmail.com>
2018-05-22 21:19:19 -0700

    Removing TODO

--- Tabless/String+WebURL.swift
+++ Tabless/String+WebURL.swift
@@ -1,6 +1,5 @@
 extension String {
     /// Returns whether the string looks like a web URL
-    /// TODO(#2): Better detect URLs
     func isWebURL() -> Bool {
         return hasPrefix("http")
             || hasPrefix("www.")
