9655bb7630336d84fe88536da79136962164bdc5
Eric Bomgardner <eric.bomgardner@gmail.com>
2019-09-08 13:10:05 -0700

    some fading or something

--- Tabless/AppDelegate.swift
+++ Tabless/AppDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    var view: UIView?
     var stateClearer: StateClearer!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = navigationController
+        window.backgroundColor = .white
         self.window = window
 
         window.makeKeyAndVisible()
@@ -25,7 +27,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         stateClearer.beginClearTimer()
     }
 
+    func applicationWillResignActive(_ application: UIApplication) {
+        if let window = window, let view = (window.rootViewController as? UINavigationController)?.viewControllers.last?.view {
+            UIView.animate(withDuration: 0.15) {
+                view.alpha = 0.06
+            }
+            self.view = view
+        }
+    }
+
     func applicationDidBecomeActive(_ application: UIApplication) {
+        UIView.animate(withDuration: 0.25) {
+            self.view?.alpha = 1.0
+        }
         stateClearer.cancelPendingStateClears()
     }
 }
