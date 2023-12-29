f04305d46bf0fab5d1cac0326c9ae50fc8aceb78
Eric Bomgardner <eric.bomgardner@gmail.com>
2023-12-29 17:13:14 -0500

    Revert "Adding button to open settings"
    This reverts commit c4f3705d3fbe70abca5e49ade714607f89035a27.

--- Tabless/Views/RootView.swift
+++ Tabless/Views/RootView.swift
@@ -5,15 +5,6 @@ class RootView: UIView {
 
     let searchView = SearchView()
     let tView = UILabel()
-    private lazy var settingsButton: UIButton = {
-        let button = UIButton(type: .custom)
-        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 18)
-        let settingsIcon = UIImage(systemName: "gearshape.fill", withConfiguration: symbolConfig)
-        button.setImage(settingsIcon, for: .normal)
-        button.tintColor = UIColor.lightGray
-        button.addTarget(self, action: #selector(openSettings), for: .touchUpInside)
-        return button
-    }()
 
     private var tViewTopConstraint: NSLayoutConstraint!
     private(set) var searchViewBottomConstraint: NSLayoutConstraint!
@@ -28,18 +19,13 @@ class RootView: UIView {
         backgroundColor = UIColor(named: "Background")
 
         addSubview(tView)
-        addSubview(settingsButton)
         addSubview(searchView)
 
         tView.translatesAutoresizingMaskIntoConstraints = false
-        tViewTopConstraint = tView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 80)
+        tViewTopConstraint = tView.topAnchor.constraint(equalTo: topAnchor, constant: 80)
         tViewTopConstraint.isActive = true
         tView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         updateTextSize()
-
-        settingsButton.translatesAutoresizingMaskIntoConstraints = false
-        settingsButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 4).isActive = true
-        settingsButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
 
         searchView.translatesAutoresizingMaskIntoConstraints = false
         searchView.leadingAnchor.constraint(equalTo: readableContentGuide.leadingAnchor).isActive = true
@@ -61,12 +47,6 @@ class RootView: UIView {
     private func updateTextSize() {
         let tViewTextSize: CGFloat = traitCollection.isLarge ? 80 : 64
         tView.font = UIFont.boldSystemFont(ofSize: tViewTextSize)
-    }
-
-    @objc private func openSettings() {
-        if let url = URL(string: UIApplication.openSettingsURLString) {
-            UIApplication.shared.open(url)
-        }
     }
 
     // MARK: Sizing
