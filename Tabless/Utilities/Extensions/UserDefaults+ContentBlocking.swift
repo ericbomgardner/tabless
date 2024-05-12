a9e244e060548b3574e9e200f8ebc9e661b1b003
Eric Bomgardner <eric.bomgardner@gmail.com>
2024-05-11 20:06:43 -0400

    Swap defaults for content blocking and inline media playback

--- Tabless/Utilities/Extensions/UserDefaults+ContentBlocking.swift
+++ Tabless/Utilities/Extensions/UserDefaults+ContentBlocking.swift
@@ -3,6 +3,6 @@ import Foundation
 extension UserDefaults {
     // Matches key specified in Settings/Root.plist
     @objc dynamic var contentBlocking: Bool {
-        return object(forKey: #function) as? Bool ?? true
+        return object(forKey: #function) as? Bool ?? false
     }
 }
--- Tabless/Utilities/Extensions/UserDefaults+InlineMediaPlayback.swift
+++ Tabless/Utilities/Extensions/UserDefaults+InlineMediaPlayback.swift
@@ -3,6 +3,6 @@ import Foundation
 extension UserDefaults {
     // Matches key specified in Settings/Root.plist
     @objc dynamic var inlineMediaPlayback: Bool {
-        return object(forKey: #function) as? Bool ?? false
+        return object(forKey: #function) as? Bool ?? true
     }
 }
